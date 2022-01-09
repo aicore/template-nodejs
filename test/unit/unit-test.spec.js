f611c52e6fd74288d39350a5bc521c6da0df024d
Arun Bose <arun@core.ai>
2022-01-09 22:53:12 +0530

    feat: husky pre commit and push validations, commit linter

--- test/unit/unit-test.spec.js
+++ test/unit/unit-test.spec.js
@@ -9,6 +9,7 @@
 // we use c8 for coverage https://github.com/bcoe/c8. Its reporting is based on nyc, so detailed docs can be found
 // here: https://github.com/istanbuljs/nyc ; We didn't use nyc as it do not yet have ES module support
 // see: https://github.com/digitalbazaar/bedrock-test/issues/16 . c8 is drop replacement for nyc coverage reporting tool
+/*global describe, it*/
 
 import helloWorld from "../../src/index.js";
 import * as assert from 'assert';
